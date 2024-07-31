cc8a541d50e2ba6d2c1d56d0ea52dbb60dad3743
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-07-31 19:17:42 -0300

    feat: implements prisma repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
+import { PrismaQuestionsAttachmentsRepository } from "./prisma/repositories/prisma-questions-attachments-repository";
+import { PrismaQuestionsCommentsRepository } from "./prisma/repositories/prisma-questions-comments-repository";
+import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
+import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { PrismaAnswersRepository } from "./prisma/repositories/prisma-answers-repository";
+import { PrismaQuestionsRepository } from "./prisma/repositories/prisma-questions-repository";
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService]
+  providers: [PrismaService, PrismaQuestionsAttachmentsRepository, PrismaQuestionsCommentsRepository, PrismaAnswerAttachmentsRepository, PrismaAnswerCommentsRepository, PrismaAnswersRepository, PrismaQuestionsRepository],
+  exports: [PrismaService, PrismaQuestionsAttachmentsRepository, PrismaQuestionsCommentsRepository, PrismaAnswerAttachmentsRepository, PrismaAnswerCommentsRepository, PrismaAnswersRepository, PrismaQuestionsRepository]
 })
 export class DatabaseModule {}  
